affcdd7db8a4f341c5b02386567f61692eec50f0
三角拓真 <doraepon2216@gmail.com>
2015-07-31 20:04:32 +0900

    edit

--- css-wapuu-shortcord.php
+++ css-wapuu-shortcord.php
@@ -16,7 +16,6 @@ add_action('wp_head', 'css_wapuu_shortcord_stylesheet');
 
 function css_wapuu_shortcord($atts, $content=null) {
 	return '<div class="css-wapuu">
-<!-- tail・line/draw しっぽ・線/塗り -->
 <div class="line-tail1"></div>
 <div class="line-tail2"></div>
 <div class="tail1"></div>
@@ -24,18 +23,15 @@ function css_wapuu_shortcord($atts, $content=null) {
 <div class="tail3"></div>
 <div class="tail4"></div>
 
-<!-- line 線 -->
 <div class="line1"></div>
 <div class="line2"></div>
 
-<!-- ball white-line ボール・wを囲む白線 -->
 <div class="ball"></div>
 <div class="ball2"></div>
 <div class="boll-light"></div>
 <div class="w-circle"></div>
 <div class="w-circle-inner"></div>
 
-<!-- w -->
 <div class="w-1"></div>
 <div class="w-2"></div>
 <div class="w-3"></div>
@@ -50,49 +46,37 @@ function css_wapuu_shortcord($atts, $content=null) {
 <div class="w-12"></div>
 <div class="w-13"></div>
 
-<!-- face・line/draw 顔・線 -->
 <div class="line-face1"></div>
 <div class="line-face2"></div>
 
-<!-- hands・line 両手・線 -->
 <div class="line-right-hand"></div>
 <div class="line-left-hand"></div>
 
-<!-- left-leg・line 左脚・線 -->
 <div class="line-left-leg"></div>
 
-<!-- face・draw 顔・塗り -->
 <div class="face"></div>
 <div class="neck"></div>
 
-<!-- body・draw 胴・塗り -->
 <div class="body"></div>
 <div class="belly"></div>
 
-<!-- arm・draw 腕・塗り -->
 <div class="left-hand"></div>
 <div class="right-hand"></div>
 <div class="arm"></div>
 
-<!-- right-foot・line/draw 右足・線/塗り -->
 <div class="line-right-foot"></div>
 <div class="right-foot"></div>
 
-<!-- left-leg・draw 左脚・塗り -->
 <div class="left-leg"></div>
-<!-- left-foot・line/draw 左足・線/塗り -->
 <div class="line-left-foot"></div>
 <div class="left-foot"></div>
 <div class="thenar"></div>
 
-<!-- left-ear・line/draw 左耳・線/塗り -->
 <div class="line-left-ear"></div>
 <div class="ear-left"></div>
 
-<!-- right-ear・draw 右耳・塗り -->
 <div class="ear-right"></div>
 
-<!-- face-parts 顔パーツ -->
 <div class="eye-rignt blink"></div>
 <div class="eye-left blink"></div>
 <div class="nose"></div>
